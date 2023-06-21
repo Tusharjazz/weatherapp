9acbf093cb129e27d820cb395ac6b341f063ba23
Tushar Ranjan <tushar.r@lemonaidegroup.com>
2023-06-21 17:46:07 +0700

    first commit

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex flex-col flex-1 items-center">
+    <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
+    <p>You are currently previewing this city, click the '+' icon to start tracking this city</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { useRoute } from "vue-router";
+const route = useRoute();
+
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat={route.query.lat}&lon={route.query.lng}&exclude={part}&appid=f959c39c40831b8a2bf897afbe082f0b&units=imperial`
+    );
+
+    // calculate current date and time
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = weatherData.data.current.dt * 1000 + localOffset;
+    weatherData.data.currentTime =
+      utc + 1000 * weatherData.data.timezone_offset;
+
+    // calculate hourly weather offset
+    weatherData.data.hourly.forEach((hour) => {
+      const utc = hour.dt * 1000 + localOffset;
+      hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+    });
+    return weatherData;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const weatherData = await getWeatherData();
+console.log(weatherData);
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,80 @@
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        v-model="searchQuery"
+        type="text"
+        @input="getSearchResults"
+        placeholder="Search for a city or province"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <p v-if="searchError">Sorry, something went wrong. Please try again.</p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0">No results match your query. Please try a different term.</p>
+        <template v-else>
+          <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+          @click="previewCity(searchResult)"
+        >
+          {{ searchResult.place_name }}
+        </li>
+        </template>
+        
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter()
+
+const mapboxAPIKey =
+  "pk.eyJ1IjoidHVzaGFyamF6eiIsImEiOiJjbGo1ZXlkZmEwN3JmM3FwM29qdGtsc3h4In0.p_yfe5BRREbCxw579qNacA";
+
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, province] = searchResult.place_name.split(',')
+  router.push({
+    name: 'cityView',
+    params: {province: province.replaceAll(" ", ""), city:city},
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    }
+  })
+}
+
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
+        mapboxSearchResults.value = result.data.features;
+      } catch {
+        searchError.value = true;
+      }
+
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+</script>
